e38e0c7588635397470cc724fd0edabecdba5431
moriizo <mori-izo@g.ecc.u-tokyo.ac.jp>
2024-12-10 01:43:58 +0900

    [update] Add base path

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }) => {
+  let base = "/";
+
+  if (mode === "production") {
+    base = "/75/recap/";
+  }
+
+  return {
+    plugins: [react()],
+    base: base,
+  };
+});
